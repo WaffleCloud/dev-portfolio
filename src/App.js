ebea2aa8332416f584a66266586133142cb070a0
Rilo <taydh.wade@gmail.com>
2023-11-25 13:05:56 -0800

    fixed nav bar positioning

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Bg from './Bg'
+import Intro from './Intro'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="app">
+      <Bg/>
+      <Nav/>
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+
+
+const Nav = () => {
+    return (
+        <div className="nav-container">
+            <div className="nav-button">Home</div>
+            <div className="nav-button">Bio</div>
+            <div className="nav-button">Websites</div>
+            <div className="nav-button">Contact</div>
+        </div>
+    )
+}
+
+export default Nav
